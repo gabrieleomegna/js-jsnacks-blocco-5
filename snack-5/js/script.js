a251f421f31898b6c7d9177adb970efd6e5a3db0
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-12-04 22:21:37 +0100

    Snack 5

---
+++ snack-5/js/script.js
@@ -0,0 +1,5 @@
+// Creare un array di oggetti di squadre di volley.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+// Generare numeri random al posto delle nelle proprietà:
+// punti fatti e falli subiti
+
